b1e1e15745baa0a20e761e445850796ed3f206f5
vrglab <vrglab1@gmail.com>
2022-03-25 18:13:35 +0100

    Changed the grid system

--- Scripts/CDate.js
+++ Scripts/CDate.js
@@ -1,9 +1,15 @@
 //Set the header With the month name and date
+
+//Get the date
 var now = new Date();
+//Convert it to string
 var nowStr = now.toDateString(); 
 
+//Split the string
 var currentMonth = nowStr.split(' '); 
 
+
+//Set the text data of the header to the month name and date
 document.getElementById("Month").innerText = currentMonth[1] + " " + currentMonth[2];
 
 
@@ -12,31 +18,20 @@ let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("cell");
 
 // Creates a default grid sized 16x16
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
-}
-
-// Takes (rows, columns) input and makes a grid
-function makeRows(rowNum) {
-
-    // Creates rows
-    for (r = 0; r < rowNum; r++) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
-    };
-};
-
-// Creates columns
-function makeColumns(cellNum) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = "cell";
-        };
-
-    };
-};
-
-
-defaultGrid();
+window.addEventListener("load", () => {
+    // (B1) DATA ARRAY
+    var data = [
+      ["A", "B", "C"],
+      ["D", "E", "F"],
+      ["G", "H", "I"]
+    ];
+   
+    // (B2) LOOP + ADD CELLS
+   
+    for (let i of data) { for (let j of i) {
+      let cell = document.createElement("div");
+      cell.innerHTML = j;
+      cell.className = "cell";
+      container.appendChild(cell);
+    }}
+  });
